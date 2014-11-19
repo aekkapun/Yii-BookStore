562ef95e18b0ec0874ca96212cf5c017a8e27d7d
Ilia Marchenko <il-marc@ya.ru>
2014-11-19 15:49:07 +0200

    Book view

--- views/book/view.php
+++ views/book/view.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="book-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?php if (!Yii::$app->user->isGuest): ?>
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->id], [
@@ -24,14 +24,72 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]) ?>
     </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'title',
-            'cover',
-        ],
-    ]) ?>
+    <?php endif; ?>
+    <table class="table table-striped table-bordered detail-view">
+        <tr>
+            <th>
+                ID
+            </th>
+            <td>
+                <?= Html::encode($model->id) ?>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                Title
+            </th>
+            <td>
+                <?= Html::encode($model->title) ?>
+            </td>
+        </tr>
+        <tr>
+            <th>
+                Cover
+            </th>
+            <td>
+                <?= Html::img('/uploads/covers/'.$model->cover) ?>
+            </td>
+        </tr>
+        <?php if($model->authors):?>
+        <tr>
+            <th>
+                Author<?=count($model->authors)>1?'s':null?>
+            </th>
+            <td>
+                <?php
+                    $authors = $model->authors;
+                    end($authors);
+                    $lastAuthorKey = key($authors);
+                ?>
+                <?php foreach($model->authors as $key => $author): ?>
+                    <?= Html::a(
+                        $author->first_name.' '.$author->second_name,
+                        ['author/view', 'id' => $author->id]
+                    ).($key != $lastAuthorKey?', ':null) ?>
+                <?php endforeach ?>
+            </td>
+        </tr>
+        <?php endif ?>
+        <?php if($model->subjects):?>
+        <tr>
+            <th>
+                Subject<?=count($model->subjects)>1?'s':null?>
+            </th>
+            <td>
+                <?php
+                    $subjects = $model->subjects;
+                    end($subjects);
+                    $lastSubjectKey = key($subjects);
+                ?>
+                <?php foreach($model->subjects as $key => $subject): ?>
+                    <?= Html::a(
+                        $subject->name,
+                        ['subject/view', 'id' => $subject->id]
+                    ).($key != $lastSubjectKey?', ':null) ?>
+                <?php endforeach ?>
+            </td>
+        </tr>
+        <?php endif ?>
+    </table>
 
 </div>
